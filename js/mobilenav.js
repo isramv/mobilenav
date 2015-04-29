eefd1123b667bd8152184bffaf0fe34f66f90a6a
Israel Morales <israel@ooyala.com>
2015-04-29 15:27:03 -0500

    swapped jQuery selector in the js/mobilenav.js file

--- js/mobilenav.js
+++ js/mobilenav.js
@@ -2,27 +2,27 @@
     Drupal.behaviors.mobilenav = {
         attach: function (context, settings) {
             var toggler = "<div class='toggler'></div>";
-            jQuery(toggler).insertAfter(jQuery('#block-mobilenav-mobilenavigation ul.menu > li.expanded > a'));
-            body = jQuery('body');
-            sidebar = jQuery('#block-mobilenav-mobilenavigation');
-            jQuery(sidebar).height(jQuery(window).height());
-            jQuery(window).resize(function () {
-                if (jQuery(window).width() > Drupal.settings.mobilenav.breakpoint) {
+            $(toggler).insertAfter($('#block-mobilenav-mobilenavigation ul.menu > li.expanded > a'));
+            body = $('body');
+            sidebar = $('#block-mobilenav-mobilenavigation');
+            $(sidebar).height($(window).height());
+            $(window).resize(function () {
+                if ($(window).width() > Drupal.settings.mobilenav.breakpoint) {
                     body.removeClass('expanded').addClass('hide');
                 }
-                jQuery(sidebar).height(jQuery(window).height());
+                $(sidebar).height($(window).height());
             });
-            jQuery(body).height(jQuery(window).height());
-            jQuery('#burger').click(function (event) {
-                if(jQuery(body).hasClass('expand')) {
-                    jQuery(body).removeClass('expand').addClass('hide');
+            $(body).height($(window).height());
+            $('#burger').click(function (event) {
+                if($(body).hasClass('expand')) {
+                    $(body).removeClass('expand').addClass('hide');
                 } else {
-                    jQuery(body).addClass('expand').removeClass('hide');
+                    $(body).addClass('expand').removeClass('hide');
                 }
             });
-            jQuery('.oosidebar li.expanded > a').bind('click', function (e) {
+            $('.oosidebar li.expanded > a').bind('click', function (e) {
                 e.preventDefault();
-                $elem = jQuery(this);
+                $elem = $(this);
                 console.log($elem);
                 if ($elem.next().next().is(':visible')) {
                     $($elem).next().next().slideUp();
@@ -32,9 +32,9 @@
                     $($elem).next().next().slideDown();
                 }
             });
-            jQuery('.oosidebar .toggler').bind('click', function() {
+            $('.oosidebar .toggler').bind('click', function() {
                 elem = this;
-                parentelement = jQuery(elem.parentElement);
+                parentelement = $(elem.parentElement);
                 if(parentelement.hasClass('closed')) {
                     parentelement.removeClass('closed').addClass('open');
                 } else {
@@ -42,7 +42,7 @@
                 }
 
 
-                element = jQuery(elem.nextElementSibling);
+                element = $(elem.nextElementSibling);
                 if(element.hasClass('open')) {
                     element.removeClass('open');
                     element.slideUp();
